be5f14ecd288d39297a521f757427c30ed27bd20
Dakoina <debondtjan@gmail.com>
2021-07-09 16:43:55 +0200

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCnUD4J5HMBeBoLoov0bWmn5VRw-TfQqO4",
+    authDomain: "game-night-organizer-8479a.firebaseapp.com",
+    databaseURL: "https://game-night-organizer-8479a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "game-night-organizer-8479a",
+    storageBucket: "game-night-organizer-8479a.appspot.com",
+    messagingSenderId: "916117888932",
+    appId: "1:916117888932:web:5ccef6e3f3d8c01b032b31"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCnUD4J5HMBeBoLoov0bWmn5VRw-TfQqO4",
+    authDomain: "game-night-organizer-8479a.firebaseapp.com",
+    databaseURL: "https://game-night-organizer-8479a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "game-night-organizer-8479a",
+    storageBucket: "game-night-organizer-8479a.appspot.com",
+    messagingSenderId: "916117888932",
+    appId: "1:916117888932:web:5ccef6e3f3d8c01b032b31"
+
+  }
 };
 
 /*
